3621289dc9c87b62824fb2cc74b63381ecb5eded
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-09-08 21:02:16 +0800

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Xl     [6287793236775]
+│ • Gopay  [6285338890541]
+│ • tlkm   [6282147562437]
+│ • Saweria  [https://saweria.co/sxylbotz9]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6287793236775
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
